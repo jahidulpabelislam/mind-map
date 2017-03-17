f91fa1d9ac063a93505913986b6436cfbf48ae2e
Ade Crown <adecrown1@live.co.uk>
2017-03-17 13:47:45 +0000

    Fix link error

--- main.js
+++ main.js
@@ -23,7 +23,64 @@ var data = {
     nodes: nodes,
     edges: edges
 };
-var options = {};
+
+
+var options = {
+
+physics:{
+    enabled: false,
+    stabilization: false,
+},
+  interaction:{
+    dragNodes:true,
+    dragView: true,
+    hover: true,
+    hoverConnectedEdges: true,
+    keyboard: {
+      enabled: true,
+      speed: {x: 10, y: 10, zoom: 0.02},
+      bindToWindow: true
+    },
+    multiselect: true,
+    navigationButtons: true,
+    selectable: true,
+    selectConnectedEdges: true,
+    tooltipDelay: 300,
+    zoomView: true
+  },
+  manipulation: {
+    enabled: true,
+    initiallyActive: true,
+    addNode: function(nodeData,callback)
+    {
+      addNode(nodeData,callback)
+    },
+    editNode: function(nodeData,callback)
+    {
+      addNode(nodeData,callback)
+    },
+    addEdge: true,
+    //editNode: true,
+    editEdge: true,
+    deleteNode: true,
+    deleteEdge: true,
+    controlNodeStyle:{
+      // all node options are valid.
+    },
+
+  },
+
+  "edges": {
+      smooth: false,
+    "arrows": {
+      "to": {
+        "enabled": true
+      }
+    }
+  },
+
+};
+
 
 // initialize your network!
 var network = new vis.Network(container, data, options);
